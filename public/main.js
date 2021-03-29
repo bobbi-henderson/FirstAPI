adddb0899440f597ed4ced5828171a958bbadbb0
Bobbi Henderson <bobbilhenderson@gmail.com>
2021-03-28 19:23:51 -0500

    Initial commit, includes Get, Post and Delete routes.

---
+++ public/main.js
@@ -0,0 +1,47 @@
+const getBook = document.querySelector('#getBook')
+const bookTitle = document.querySelector('#bookTitle')
+const bookAuthor = document.querySelector('#bookAuthor')
+const bookDescription = document.querySelector('#bookDescription')
+const deleteBook = document.querySelector('#deleteBook')
+
+getBook.addEventListener('click', getRandomBook)
+
+deleteBook.addEventListener('click', removeBook)
+
+async function getRandomBook(){
+    try {
+        const res = await fetch('http://localhost:8000/api/books')
+        const data = await res.json()
+        console.log(data)
+
+        let books = Object.keys(data)
+        let rand = Math.floor(Math.random()*books.length)
+        
+        let book = [books[rand]]
+        
+        bookTitle.innerText = data[book].title
+        bookAuthor.innerText = data[book].author
+        bookDescription.innerText = data[book].description
+        deleteBook.name = data[book].title
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+async function removeBook(){
+    const title = deleteBook.name
+    try {
+        const response = await fetch('deleteBook', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'title': title
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    } catch(err){
+        console.log(err)
+    }
+}
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const cors = require('cors')
+const PORT = 8000
+
+let db,
+  dbConnectionStr = 'mongodb+srv://yoda:StarW@rs@cluster0.cvovs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  dbName = 'book'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    db = client.db(dbName)
+  })
+  .catch(error => console.error(error))
+
+
+app.set('view engine', 'ejs')
+app.use(cors())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    db.collection('books').find().toArray()
+    .then(data =>{
+      response.render('index.ejs', {})
+    })
+    .catch(error => console.error(error))
+})
+
+app.post('/addBook', (request, response)=>{
+    db.collection('books').insertOne(request.body)
+    .then(result =>{
+      console.log(result)
+      response.redirect('/')
+    })
+    .catch(error => console.error(error))
+})
+
+app.delete('/deleteBook', (request, response)=>{
+  db.collection('books').deleteOne({title: request.body.title})
+  .then(result =>{
+    console.log('Book Deleted')
+    response.json('Book Deleted')
+  })
+  .catch(error => console.error(error))
+})
+
+app.get('/api/books', (request, response) => {
+  db.collection('books').find().toArray()    
+  .then(data =>{
+    response.json(data)
+  })
+  .catch(error => console.error(error))
+})
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
